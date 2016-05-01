16c30318865e12e2014910ceeecdaf7a2588a64c
Basile Vu <basile.vu@gmail.com>
2016-05-01 22:37:17 +0200

    Cleaning a bit

--- src/main.cpp
+++ src/main.cpp
@@ -17,40 +17,42 @@ int main () {
         return (25 + x * (x - 6) * (x - 8) * (x - 14) / 25) * exp(sqrt(1 + cos(x*x / 10)));
     };
 
+    double a = 0, b = 15;
+    size_t M = 10000, N = 1000000;
+    size_t numPointsPWL = 30;
+
     seed_seq seed = {24, 512, 42};
 
     {
-        // ~ 601.989
         UniformSampling us(g);
         us.setSeed(seed);
 
         clock_t start = clock();
-        MonteCarloMethod::Sampling s = us.sample(100000, 0, 15);
+        MonteCarloMethod::Sampling s = us.sample(N, a, b);
 
         cout << s.areaEstimator << ", " << s.confidenceInterval.toString() << endl;
         cout << (double) (clock() - start) / CLOCKS_PER_SEC << "s" << endl;
     }
 
-    Points points = Stats::createPoints(30, g, 0, 15);
+    Points points = Stats::createPoints(numPointsPWL, g, a, b);
 
     {
         ImportanceSampling is(g);
         is.setSeed(seed);
 
         clock_t start = clock();
-        MonteCarloMethod::Sampling s = is.sample(100000, points.xs, points.ys);
+        MonteCarloMethod::Sampling s = is.sample(N, points.xs, points.ys);
 
         cout << s.areaEstimator << ", " << s.confidenceInterval.toString() << endl;
         cout << (double) (clock() - start) / CLOCKS_PER_SEC << " s" << endl;
     }
 
     {
-
         ControlVariable cv(g);
         cv.setSeed(seed);
 
         clock_t start = clock();
-        MonteCarloMethod::Sampling s = cv.sample(10000, 1000000, 0, 15, points.xs, points.ys);
+        MonteCarloMethod::Sampling s = cv.sample(M, N, a, b, points.xs, points.ys);
 
         cout << s.areaEstimator << ", " << s.confidenceInterval.toString() << endl;
         cout << (double) (clock() - start) / CLOCKS_PER_SEC << " s" << endl;
